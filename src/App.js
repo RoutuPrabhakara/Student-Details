2e9c1dfc3fcb6868d34b1432559900ae5a86bc6b
RoutuPrabhakara <routuprabhakar2000@gmail.com>
2024-01-30 22:31:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App(){
       MajorSkills:"Python full stock",
       OtherSkills:"Reading",
       Hobbies:"Suport Team ",
+      Support:"Self",
       Experience:"Cloud Developer",
       profile:"https://cdn2.f-cdn.com/files/download/38545966/4bce6b.jpg",
       
@@ -32,6 +33,7 @@ function App(){
       MajorSkills:"Java full stock",
       OtherSkills:"Singing",
       Hobbies:"Helping Nature ",
+      Support:"Self",
       Experience:".Net Developer",
       profile:"https://th.bing.com/th/id/OIP.eteuNjb-uZCqYoeT3BFkvAHaE8?pid=ImgDet&w=474&h=316&rs=1"
       
@@ -46,6 +48,7 @@ function App(){
       MajorSkills:"React full stock",
       OtherSkills:"cricket",
       Hobbies:"Whatching Movies ",
+      Support:"Self",
       Experience:"Frontend Developer",
       profile:"https://th.bing.com/th/id/OIP.IoUmyjrf4VaXudyiVqv2WwHaII?pid=ImgDet&w=474&h=520&rs=1",
       
